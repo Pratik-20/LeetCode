b597730babaca153aeb9cf0b07a009960e5392f2
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2023-02-14 13:30:53 +0530

    Update 1523_Count_Odd_Numbers_in an_Interval_Range.py

--- 1523_Count_Odd_Numbers_in an_Interval_Range.py
+++ 1523_Count_Odd_Numbers_in an_Interval_Range.py
@@ -25,7 +25,7 @@ class Solution:
     def countOdds(self, low: int, high: int) -> int:
         return ceil(high/2)-ceil((low-1)/2)
 
-
+# Solution - 1 # Working fine 
 
 """
 class Solution:
@@ -44,4 +44,17 @@ class Solution:
     
         elif low % 2 == 0 and high%2 ==1:
             return int(((high - low)/2)+1)
-"""        
+"""
+
+Solution 2 - Time Limit Exceed Error
+
+"""
+
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        counter =0
+        for i in range(low,high+1):
+            if i%2 == 1:
+                counter += 1
+        return counter
+"""
