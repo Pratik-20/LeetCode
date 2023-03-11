bca4bef53921451cdc2a31158db85652f5c2d72b
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2023-03-11 19:40:06 +0530

    Add files via upload

---
+++ SS.py
@@ -0,0 +1,11 @@
+import pyautogui
+from datetime import datetime
+
+now = datetime.now()
+
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")
+print("date and time =", dt_string)
+
+myScreenshot = pyautogui.screenshot()
+myScreenshot.save(r'C:\Users\prati\OneDrive\Desktop\New folder (5)\Screenshot_'+dt_string+'.png')
